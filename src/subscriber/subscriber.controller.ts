f63c34b88e25f086b4b6a40fa204e6113d0cba27
Mbouna Ulrich <60409730+Ulrich15@users.noreply.github.com>
2023-11-05 06:55:40 +0100

    API 19
    Fix du sous de payload dans l'enregistrement d'un subcribers

--- src/subscriber/subscriber.controller.ts
+++ src/subscriber/subscriber.controller.ts
@@ -1,7 +1,12 @@
 import { Controller } from '@nestjs/common';
 import { SubscriberService } from './subscriber.service';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
-import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
+import {
+  Ctx,
+  MessagePattern,
+  RmqContext,
+  Payload,
+} from '@nestjs/microservices';
 
 @Controller('subscribers')
 export class SubscriberController {
@@ -9,7 +14,10 @@ export class SubscriberController {
 
   // @MessagePattern({ cmd: 'add-subscriber' })
   @MessagePattern({ cmd: 'add-subscriber' })
-  addSubscriber(subscriber: CreateSubscriberDto, @Ctx() context: RmqContext) {
+  addSubscriber(
+    @Payload() subscriber: CreateSubscriberDto,
+    @Ctx() context: RmqContext,
+  ) {
     const newSubscriber = this.subscriberService.addSubscriber(subscriber);
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
